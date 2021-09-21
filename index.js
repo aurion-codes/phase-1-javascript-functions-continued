5c407fb3aa30ddb7a295a40df5ce66e43fed4ab2
AurionHoward <aurionhoward@gmail.com>
2021-09-21 18:26:05 -0400

    commit tree 9411bf366239fa9f613009b79114dd3036433dc7

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// Your code here
+let activity = 'roller-skate'
+function saturdayFun(activity){
+    if (activity = 'roller-skate') {
+return "This Saturday, I want to roller-skate!";
+}   else {
+        return 'This Saturday, I want to bathe my dog!'
+}
+}
+
+
+
+    
